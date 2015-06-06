cd5e79b42d6c793a2d6c06b0d6fe92d53f007dc5
fellyph <fellyph.cintra@gmail.com>
2015-06-06 17:04:07 -0300

    Change archive formatting

--- v1/js/game.js
+++ v1/js/game.js
@@ -1,38 +1,35 @@
 	
 	var gameConfig = { 
-			level:[
-				{
-					rows:8,
-					cols:8,
-					bombsNum:10
+			level: [{
+					rows: 8,
+					cols: 8,
+					bombsNum: 10
 				},
 				{	
-					rows:12,
-					cols:12,
-					bombsNum:30
+					rows: 12,
+					cols: 12,
+					bombsNum: 30
 				},
 				{	
-					rows:18,
-					cols:18,
-					bombsNum:50
+					rows: 18,
+					cols: 18,
+					bombsNum: 50
 				}],
-			currentLevel:1,
-			width:50,
-			height:50,
-			spaceBox:3,
-			color:"#000000",
+			currentLevel: 1,
+			width: 50,
+			height: 50,
+			spaceBox: 3,
+			color:'#000000',
 			bombsList:[]
-	}
-		
-		var ctx,
-			canvas,
-			countrows = 0,
-			countcols = 0,
-			mouseX,
-			mouseY,
-			youlose = false;
-			
-		var boxesToCheck = [
+    },
+        ctx,
+        canvas,
+        countrows = 0,
+        countcols = 0,
+        mouseX,
+        mouseY,
+        youlose = false,
+        boxesToCheck = [
 				[1,1],
 				[0,1],
 				[-1,1],
@@ -42,43 +39,41 @@
 				[1,-1],
 				[1,0]
 			],
-			clickedBases = [];
+        clickedBases = [];
 			
-		window.addEventListener("load",function(){
-			 canvas = document.getElementById("my-canvas");
-			 ctx = canvas.getContext("2d");
+		window.addEventListener('load', function() {
+			 canvas = document.getElementById('my-canvas');
+			 ctx = canvas.getContext('2d');
 			 canvas.width = gameConfig.width * gameConfig.level[gameConfig.currentLevel].cols;
 			 canvas.height = gameConfig.height * gameConfig.level[gameConfig.currentLevel].rows;
 			 initGame()
 		})
 		
-		function onClickGame(e){
-			mouseX = e.offsetX;
-			mouseY = e.offsetY;
+		function onClickGame(event){
+			mouseX = event.offsetX;
+			mouseY = event.offsetY;
 			
-			clickedX = Math.floor(mouseX/gameConfig.width);
-			clickedY = Math.floor(mouseY/gameConfig.height);
-			console.log(clickedX + "," + clickedY);
+			clickedX = Math.floor(mouseX / gameConfig.width);
+			clickedY = Math.floor(mouseY / gameConfig.height);
 			
-			for(var i = 0; i< gameConfig.level[gameConfig.currentLevel].bombsNum; i++){
+			for(var i = 0; i < gameConfig.level[gameConfig.currentLevel].bombsNum; i++){
 				if(clickedX == gameConfig.bombsList[i][0] && clickedY == gameConfig.bombsList[i][1]){
-					gameOver()
+					gameOver();
 				}
 			}
 				
 			if(youlose == false){
-				console.log("you live")
-				youStillAlive()
+				youStillAlive();
 			}
 		}
 	
-		function initGame(){
+		function initGame() {
 			drawStage();
 			sortBombs();
 			canvas.addEventListener("click", onClickGame);
 		}
 		
-		function sortBombs(){
+		function sortBombs() {
 			for(var i=0 ; i < gameConfig.level[gameConfig.currentLevel].bombsNum; i++){
 				gameConfig.bombsList.push([
 					Math.floor(Math.random()*gameConfig.level[gameConfig.currentLevel].cols),
@@ -87,8 +82,7 @@
 			}
 		}
 		
-		function gameOver(){
-			console.log("you died")
+		function gameOver() {
 			youlose = true;
 			
 			for(i = 0; i < gameConfig.bombsList.length; i++){
@@ -101,7 +95,7 @@
 			document.getElementById("game").className += " gameover";
 		}
 		
-		function youStillAlive(){
+		function youStillAlive() {
 			var numOfNearBombs = 0;
 			
 			for(i in boxesToCheck){
@@ -112,33 +106,29 @@
 				}
 			}
 			
-			clearBox(clickedX*gameConfig.width,clickedY*gameConfig.height,numOfNearBombs)
+			clearBox(clickedX * gameConfig.width, clickedY * gameConfig.height, numOfNearBombs)
 			clickedBases[(clickedBases.length)]  = [clickedX,clickedY,numOfNearBombs];
 		}
 		
-		function clearBox(x,y,num){
+		function clearBox(x, y, num) {
 		 	ctx.clearRect(x,y,gameConfig.width,gameConfig.height);
 		 	ctx.fillStyle= "#000000";
 		 	ctx.font = "20px Arial";
 			ctx.fillText(num,x+gameConfig.width*0.4,y+gameConfig.height*0.7);	
 		}
 		
-		function checkBase(i,x,y){
-			if(gameConfig.bombsList[i][0] == x && gameConfig.bombsList[i][1] ==y){
-				return true;
-			}else{
-				return false;
-			}
+		function checkBase(i, x, y) {
+			return (gameConfig.bombsList[i][0] == x && gameConfig.bombsList[i][1] == y) ? true : false;
 		}
 		
-		function drawStage(){
+		function drawStage() {
 			ctx.clearRect(0,0,400,600);
-			var total = gameConfig.level[gameConfig.currentLevel].cols * gameConfig.level[gameConfig.currentLevel].rows;
-			console.log(total)
+			var total = gameConfig.level[gameConfig.currentLevel].cols *     
+                        gameConfig.level[gameConfig.currentLevel].rows;
 			
 			for(var i = 0; i < total; i++){
-				var x = countcols * (gameConfig.width);
-				var y = countrows * (gameConfig.height);
+				var x = countcols * (gameConfig.width),
+                    y = countrows * (gameConfig.height);
 				
 				ctx.beginPath()
 				ctx.lineWidth = 1;
@@ -148,7 +138,7 @@
 				ctx.fill();
 				ctx.stroke();
 						
-				if(countcols < gameConfig.level[gameConfig.currentLevel].cols-1){
+				if(countcols < gameConfig.level[gameConfig.currentLevel].cols - 1) {
 					countcols++;
 				}else{
 					countcols = 0;
